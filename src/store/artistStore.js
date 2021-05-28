266742c70be82f7cfcea1b34f6afd85a8590b58a
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-05-28 19:16:04 -0400

    fixed string formatting issues

--- src/store/artistStore.js
+++ src/store/artistStore.js
@@ -3,7 +3,7 @@ const artists = [
     "id": 1,
     "name": "Artist 0",
     "bio": "Sed posuere consectetur est at lobortis.",
-    "soundcloud_embed": "&lt;iframe width=\"100%\" height=\"300\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1005504319&color=%23d0c0a0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true\"&gt;&lt;/iframe&gt;<div><a href=\"https://soundcloud.com/soundcloud-scenes\" title=\"Scenes: Indie\" target=\"_blank\">Scenes: Indie</a> · <a href=\"https://soundcloud.com/soundcloud-scenes/sets/coffee-shop-mellow-indie-chill\" title=\"Mellow Indie Chill: Coffee Shop\" target=\"_blank\">Mellow Indie Chill: Coffee Shop</a></div>",
+    "soundcloud_embed": "<iframe width='100%' height='300' scrolling='no' frameborder='no' allow='autoplay' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1005504319&color=%23d0c0a0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'></iframe><div><a href='https://soundcloud.com/soundcloud-scenes' title='Scenes: Indie' target='_blank'>Scenes: Indie</a> · <a href='https://soundcloud.com/soundcloud-scenes/sets/coffee-shop-mellow-indie-chill' title='Mellow Indie Chill: Coffee Shop' target='_blank'>Mellow Indie Chill: Coffee Shop</a></div>",
     "tag": "indie, rock, rap, spoken, folk",
     "date_created": "2021-03-21T00:00:00.000Z"
   },
@@ -11,7 +11,7 @@ const artists = [
     "id": 2,
     "name": "Artist 1",
     "bio": "Sed posuere consectetur est at lobortis.",
-    "soundcloud_embed": "&lt;iframe width=\"100%\" height=\"300\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1005504319&color=%23d0c0a0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true\"&gt;&lt;/iframe&gt;<div><a href=\"https://soundcloud.com/soundcloud-scenes\" title=\"Scenes: Indie\" target=\"_blank\">Scenes: Indie</a> · <a href=\"https://soundcloud.com/soundcloud-scenes/sets/coffee-shop-mellow-indie-chill\" title=\"Mellow Indie Chill: Coffee Shop\" target=\"_blank\">Mellow Indie Chill: Coffee Shop</a></div>",
+    "soundcloud_embed": "<iframe width='100%' height='300' scrolling='no' frameborder='no' allow='autoplay' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1005504319&color=%23d0c0a0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'></iframe><div><a href='https://soundcloud.com/soundcloud-scenes' title='Scenes: Indie' target='_blank'>Scenes: Indie</a> · <a href='https://soundcloud.com/soundcloud-scenes/sets/coffee-shop-mellow-indie-chill' title='Mellow Indie Chill: Coffee Shop' target='_blank'>Mellow Indie Chill: Coffee Shop</a></div>",
     "tag": "indie, rock, rap, spoken, folk",
     "date_created": "2021-03-22T00:00:00.000Z"
   },
@@ -19,7 +19,7 @@ const artists = [
     "id": 3,
     "name": "Artist 2",
     "bio": "Sed posuere consectetur est at lobortis.",
-    "soundcloud_embed": "&lt;iframe width=\"100%\" height=\"300\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1005504319&color=%23d0c0a0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true\"&gt;&lt;/iframe&gt;<div><a href=\"https://soundcloud.com/soundcloud-scenes\" title=\"Scenes: Indie\" target=\"_blank\">Scenes: Indie</a> · <a href=\"https://soundcloud.com/soundcloud-scenes/sets/coffee-shop-mellow-indie-chill\" title=\"Mellow Indie Chill: Coffee Shop\" target=\"_blank\">Mellow Indie Chill: Coffee Shop</a></div>",
+    "soundcloud_embed": "<iframe width='100%' height='300' scrolling='no' frameborder='no' allow='autoplay' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1005504319&color=%23d0c0a0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'></iframe><div><a href='https://soundcloud.com/soundcloud-scenes' title='Scenes: Indie' target='_blank'>Scenes: Indie</a> · <a href='https://soundcloud.com/soundcloud-scenes/sets/coffee-shop-mellow-indie-chill' title='Mellow Indie Chill: Coffee Shop' target='_blank'>Mellow Indie Chill: Coffee Shop</a></div>",
     "tag": "indie, rock, rap, spoken, folk",
     "date_created": "2021-03-23T00:00:00.000Z"
   },
@@ -27,7 +27,7 @@ const artists = [
     "id": 4,
     "name": "Artist 3",
     "bio": "Sed posuere consectetur est at lobortis.",
-    "soundcloud_embed": "&lt;iframe width=\"100%\" height=\"300\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1005504319&color=%23d0c0a0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true\"&gt;&lt;/iframe&gt;<div><a href=\"https://soundcloud.com/soundcloud-scenes\" title=\"Scenes: Indie\" target=\"_blank\">Scenes: Indie</a> · <a href=\"https://soundcloud.com/soundcloud-scenes/sets/coffee-shop-mellow-indie-chill\" title=\"Mellow Indie Chill: Coffee Shop\" target=\"_blank\">Mellow Indie Chill: Coffee Shop</a></div>",
+    "soundcloud_embed": "<iframe width='100%' height='300' scrolling='no' frameborder='no' allow='autoplay' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1005504319&color=%23d0c0a0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'></iframe><div><a href='https://soundcloud.com/soundcloud-scenes' title='Scenes: Indie' target='_blank'>Scenes: Indie</a> · <a href='https://soundcloud.com/soundcloud-scenes/sets/coffee-shop-mellow-indie-chill' title='Mellow Indie Chill: Coffee Shop' target='_blank'>Mellow Indie Chill: Coffee Shop</a></div>",
     "tag": "indie, rock, rap, spoken, folk",
     "date_created": "2021-03-23T00:00:00.000Z"
   },
@@ -35,73 +35,9 @@ const artists = [
     "id": 5,
     "name": "Artist 4",
     "bio": "Sed posuere consectetur est at lobortis.",
-    "soundcloud_embed": "&lt;iframe width=\"100%\" height=\"300\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1005504319&color=%23d0c0a0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true\"&gt;&lt;/iframe&gt;<div><a href=\"https://soundcloud.com/soundcloud-scenes\" title=\"Scenes: Indie\" target=\"_blank\">Scenes: Indie</a> · <a href=\"https://soundcloud.com/soundcloud-scenes/sets/coffee-shop-mellow-indie-chill\" title=\"Mellow Indie Chill: Coffee Shop\" target=\"_blank\">Mellow Indie Chill: Coffee Shop</a></div>",
+    "soundcloud_embed": "<iframe width='100%' height='300' scrolling='no' frameborder='no' allow='autoplay' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1005504319&color=%23d0c0a0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'></iframe><div><a href='https://soundcloud.com/soundcloud-scenes' title='Scenes: Indie' target='_blank'>Scenes: Indie</a> · <a href='https://soundcloud.com/soundcloud-scenes/sets/coffee-shop-mellow-indie-chill' title='Mellow Indie Chill: Coffee Shop' target='_blank'>Mellow Indie Chill: Coffee Shop</a></div>",
     "tag": "indie, rock, rap, spoken, folk",
     "date_created": "2021-03-23T00:00:00.000Z"
-  },
-  {
-    "id": 6,
-    "name": "Artist 5",
-    "bio": "Sed posuere consectetur est at lobortis.",
-    "soundcloud_embed": "&lt;iframe width=\"100%\" height=\"300\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1005504319&color=%23d0c0a0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true\"&gt;&lt;/iframe&gt;<div><a href=\"https://soundcloud.com/soundcloud-scenes\" title=\"Scenes: Indie\" target=\"_blank\">Scenes: Indie</a> · <a href=\"https://soundcloud.com/soundcloud-scenes/sets/coffee-shop-mellow-indie-chill\" title=\"Mellow Indie Chill: Coffee Shop\" target=\"_blank\">Mellow Indie Chill: Coffee Shop</a></div>",
-    "tag": "indie, rock, rap, spoken, folk",
-    "date_created": "2021-03-23T00:00:00.000Z"
-  },
-  {
-    "id": 7,
-    "name": "Artist 6",
-    "bio": "Sed posuere consectetur est at lobortis.",
-    "soundcloud_embed": "&lt;iframe width=\"100%\" height=\"300\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1005504319&color=%23d0c0a0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true\"&gt;&lt;/iframe&gt;<div><a href=\"https://soundcloud.com/soundcloud-scenes\" title=\"Scenes: Indie\" target=\"_blank\">Scenes: Indie</a> · <a href=\"https://soundcloud.com/soundcloud-scenes/sets/coffee-shop-mellow-indie-chill\" title=\"Mellow Indie Chill: Coffee Shop\" target=\"_blank\">Mellow Indie Chill: Coffee Shop</a></div>",
-    "tag": "indie, rock, rap, spoken, folk",
-    "date_created": "2021-03-23T00:00:00.000Z"
-  },
-  {
-    "id": 8,
-    "name": "Artist 7",
-    "bio": "Sed posuere consectetur est at lobortis.",
-    "soundcloud_embed": "&lt;iframe width=\"100%\" height=\"300\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1005504319&color=%23d0c0a0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true\"&gt;&lt;/iframe&gt;<div><a href=\"https://soundcloud.com/soundcloud-scenes\" title=\"Scenes: Indie\" target=\"_blank\">Scenes: Indie</a> · <a href=\"https://soundcloud.com/soundcloud-scenes/sets/coffee-shop-mellow-indie-chill\" title=\"Mellow Indie Chill: Coffee Shop\" target=\"_blank\">Mellow Indie Chill: Coffee Shop</a></div>",
-    "tag": "indie, rock, rap, spoken, folk",
-    "date_created": "2021-03-23T00:00:00.000Z"
-  },
-  {
-    "id": 9,
-    "name": "Artist 8",
-    "bio": "Sed posuere consectetur est at lobortis.",
-    "soundcloud_embed": "&lt;iframe width=\"100%\" height=\"300\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1005504319&color=%23d0c0a0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true\"&gt;&lt;/iframe&gt;<div><a href=\"https://soundcloud.com/soundcloud-scenes\" title=\"Scenes: Indie\" target=\"_blank\">Scenes: Indie</a> · <a href=\"https://soundcloud.com/soundcloud-scenes/sets/coffee-shop-mellow-indie-chill\" title=\"Mellow Indie Chill: Coffee Shop\" target=\"_blank\">Mellow Indie Chill: Coffee Shop</a></div>",
-    "tag": "indie, rock, rap, spoken, folk",
-    "date_created": "2021-03-23T00:00:00.000Z"
-  },
-  {
-    "id": 10,
-    "name": "Artist 9",
-    "bio": "Sed posuere consectetur est at lobortis.",
-    "soundcloud_embed": "&lt;iframe width=\"100%\" height=\"300\" scrolling=\"no\" frameborder=\"no\" allow=\"autoplay\" src=\"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1005504319&color=%23d0c0a0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true\"&gt;&lt;/iframe&gt;<div><a href=\"https://soundcloud.com/soundcloud-scenes\" title=\"Scenes: Indie\" target=\"_blank\">Scenes: Indie</a> · <a href=\"https://soundcloud.com/soundcloud-scenes/sets/coffee-shop-mellow-indie-chill\" title=\"Mellow Indie Chill: Coffee Shop\" target=\"_blank\">Mellow Indie Chill: Coffee Shop</a></div>",
-    "tag": "indie, rock, rap, spoken, folk",
-    "date_created": "2021-03-23T00:00:00.000Z"
-  },
-  {
-    "id": 12,
-    "name": "Artist 11",
-    "bio": "asfddasf",
-    "soundcloud_embed": "",
-    "tag": "",
-    "date_created": "2021-05-24T22:10:32.108Z"
-  },
-  {
-    "id": 13,
-    "name": "Artist 11",
-    "bio": "asfddasf",
-    "soundcloud_embed": "",
-    "tag": "",
-    "date_created": "2021-05-24T22:11:50.529Z"
-  },
-  {
-    "id": 14,
-    "name": "Jesse",
-    "bio": "this is my bio",
-    "soundcloud_embed": "",
-    "tag": "",
-    "date_created": "2021-05-25T22:28:12.080Z"
   }
 ]
 
