c5084cd05178f789c204d84bfd979b65f766f965
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-06-08 19:25:25 -0400

    uncomment instructions

--- src/routes/ArtistCreatePage/ArtistCreatePage.js
+++ src/routes/ArtistCreatePage/ArtistCreatePage.js
@@ -39,12 +39,12 @@ export default class ArtistCreatePage extends Component {
           <h2>Create Profile</h2>
           <p>When creating your profile, remember to give the 'bio' a personal touch. Remember the reader; your fans and those who would hire you want to get a sense of your background and inspirations. And don't forget to proveide links to your social media, and contact information for getting hired.</p>
           <p>Adding tags is optional, but recommended. Tags are searchable and help people find artists who use them.</p>
-          {/* <p>Putting a SoundCloud playlist of your music is also optional, but it's a great way to let people dive right into getting know and hear your sound. If you don't know how to get the embed code, follow these steps:</p>
+          <p>Putting a SoundCloud playlist of your music is also optional, but it's a great way to let people dive right into getting know and hear your sound. If you don't know how to get the embed code, follow these steps:</p>
           <ol>
             <li>On SoundCloud, click the 'Share' button below the track/playlist that you want to embed. (An overlay will appear.)</li>
             <li>Click on the "Embed" tab of the overlay, copy the HTML code in the "Code" section.</li>
             <li>On this page, paste that HTML code into the "SoundCloud Embed" form field. (Please note, you can only have one embed on your profile at this time.)</li>
-          </ol> */}
+          </ol>
         </section>
         <section>
           <form className='ArtistCreatePage' onSubmit={this.handleSubmit}>
