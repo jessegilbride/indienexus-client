1e7f82877f49c45d2682fe88a63e8799e22d9e9f
Jesse Gilbride <jessegilbride.dev@gmail.com>
2021-06-13 02:32:03 -0400

    remove demo login

--- src/routes/LoginPage/LoginPage.js
+++ src/routes/LoginPage/LoginPage.js
@@ -23,13 +23,6 @@ export default class LoginPage extends Component {
         <h2 className='page-heading'>Login</h2>
         <p className='register-message'>Don't have an account yet? <Link to='/register' className='register-link'>Register Here</Link></p>
         <LoginForm onLoginSuccess={this.handleLoginSuccess} />
-
-        <p>sample login (from database seed):</p>
-        <p>
-          username: jwhitlock
-          <br />
-          password: swSf5Hti
-        </p>
       </section>
     )
   }
